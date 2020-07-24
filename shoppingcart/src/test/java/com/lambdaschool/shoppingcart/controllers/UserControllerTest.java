9b9a4ba3be22562c19ecf3b43614eb19e4ce1405
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-23 23:00:59 -0700

    successfully deployed my app to heroku.

--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/UserControllerTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/UserControllerTest.java
@@ -1,20 +1,29 @@
 package com.lambdaschool.shoppingcart.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.shoppingcart.models.User;
 import com.lambdaschool.shoppingcart.services.UserService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = UserController.class)
@@ -45,13 +54,46 @@ public class UserControllerTest
     }
 
     @Test
-    public void getUserById()
+    public void getUserById() throws Exception
     {
+        String apiUrl = "/users/user/{userid}";
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl,
+            "1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(rb)
+            .andExpect(status().isOk())
+            .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    public void addUser()
+    public void addUser() throws Exception
     {
+        String apiUrl = "/users/user";
+
+        User newUser = new User("cinnamon",
+            "$2y$12$5EhoDOj2oGGEhmW8UFwh6e5an6WZ/IaoDPCux0oN4x4pwLm0pj6lq",
+            "SYSTEM");
+        newUser.setUserid(2);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String userString = mapper.writeValueAsString(newUser);
+
+        Mockito.when(userService.save(any(User.class)))
+            .thenReturn(newUser);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(apiUrl)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(userString);
+
+        ResultActions mvcResult = mockMvc.perform(requestBuilder)
+            .andExpect(status().isCreated())
+            .andDo(MockMvcResultHandlers.print());
+        String testResult = mvcResult.toString();
+
+        System.out.println("Actual: " + testResult);
     }
 
     @Test
